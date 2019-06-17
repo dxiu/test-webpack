1974333571f6ea24d3f755189b529abfcb66b6b5
dingxiu <914467193@qq.com>
2019-06-17 19:27:29 +0800

    多页面打包

---
+++ src/search/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import _ from 'lodash'
+class Search extends React.Component {
+  render () {
+    return (
+      <div className="color_blue">
+        Search
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <Search/>,
+  document.getElementById('root')
+)
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,24 @@
 'use strict'
 const path = require('path')
+const glob = require('glob')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin') // 压缩js
 const OptimizeCssAssestsWebpackPlugin = require('optimize-css-assets-webpack-plugin') // 优化压缩css
 const common = require('./webpack.common')
+
+const setMAP = () => {
+  const entry = {}
+  const htmlWebpackPlugins = []
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'))
+  // entryFiles.
+  console.log('entryFiles',entryFiles)
+  return {
+    entry,
+    htmlWebpackPlugins
+  }
+}
+setMAP()
 module.exports = merge(common, {
   module: {
     rules: []
